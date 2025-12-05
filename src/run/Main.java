f9b10e44362fc94693ab927251d87fd8a60127fd
AbdulRahman Eid <ydb411759@gmail.com>
2025-12-05 20:30:24 +0200

    comment

--- src/run/Main.java
+++ src/run/Main.java
@@ -1,5 +1,16 @@
 package run;
 
+/*
+   ________________________________________________________________________
+   |@author             | AbdulRahman Eid                                 |
+   |Project name        | TaskManager                                     |
+   |Project description | A simple task management application with java  |
+   |@since              | 2025-12-05                                      |
+   |@version            | Y25M12V1                                        |
+   |Complete            | No                                              |
+   ------------------------------------------------------------------------
+*/
+
 public class Main {
     public static void main(String[] args) {
         new RunApplication();
